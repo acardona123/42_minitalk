051419c596d34667da203169711f212931a30e9f
Alex Cardona <42.alex.cardona@gmail.com>
2022-12-07 20:45:23 +0100

    FIRST PUSH

--- rendu/minitalk.h
+++ rendu/minitalk.h
@@ -6,20 +6,15 @@
 /*   By: acardona <acardona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 13:12:53 by acardona          #+#    #+#             */
-/*   Updated: 2022/12/06 04:38:20 by acardona         ###   ########.fr       */
+/*   Updated: 2022/12/07 20:44:41 by acardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# define _XOPEN_SOURCE 700 //
-# include <unistd.h>
-# include <signal.h>
-# include <stdio.h>
 # include <stdlib.h>
-# include <sys/types.h>
-# include <string.h>
+# include <unistd.h>
 # include <signal.h>
 
 int		ft_atoi(const char *nptr);
--- test.c
+++
@@ -1,7 +0,0 @@
-#include "minitalk.h"
-# include <stdio.h>
-
-int main()
-{
-	printf("%d, %d", SIGUSR1, SIGUSR2);
-}
